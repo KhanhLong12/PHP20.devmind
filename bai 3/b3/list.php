20975c834dd5a2578e12993fc1c316dac09f401f
KhanhLong12 <basturlong@gmail.com>
2019-07-31 17:06:55 +0700

    khanhlong

--- bai 3/b3/list.php
+++ bai 3/b3/list.php
@@ -1,5 +1,4 @@
 <?php 
-session_start();
 		$product = array();
 		$product[] = array(
 			'id' => 'sp1',
--- bai 3/b3/viewcart.php
+++ bai 3/b3/viewcart.php
@@ -19,17 +19,22 @@
 	if (isset($_SESSION['cart']) && $_SESSION['cart'] != null) {
 		echo "<table border='1' width='600'>";
 		echo "<tr>";
+		echo "<td>ID</td>";
 		echo "<td>tên sản phẩm</td>";
 		echo "<td>Số lượng</td>";
 		echo "<td>Giá</td>";
 		echo "<td>Thành tiền</td>";
+		echo "<td>xóa sản phẩm</td>";
 		echo "</tr>";
 		foreach ($_SESSION['cart'] as $list) {
 			echo "<tr>";
+			echo "<td>".$list['id']."</td>";
 			echo "<td>".$list['name']."</td>";
 			echo "<td>".$list['số lượng']."</td>";
 			echo "<td>".$list['price']."</td>";
 			echo "<td>".($list['số lượng']*$list['price'])."</td>";
+			echo "<td><a href='delete.php?id=".$list['id']."'>delete</a></td>";
+
 			echo "</tr>";
 		}
 		echo "</table>";
