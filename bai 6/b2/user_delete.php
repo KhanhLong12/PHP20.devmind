5a439004d0ffc074d8571abf641641d53aa9c000
KhanhLong12 <basturlong@gmail.com>
2019-08-27 23:59:46 +0700

    khanhlong

--- bai 6/b2/user_delete.php
+++ bai 6/b2/user_delete.php
@@ -2,9 +2,11 @@
 	$id = $_GET['id'];
 	// echo "$id";
 	require_once "connection2.php";
-	$query = "DELETE FROM users WHERE id=".$id;
+	$query = 'DELETE FROM posts WHERE user_id= '.$id;
+	$query_ = "DELETE FROM users WHERE id=".$id;
 	// echo "$query";
-	$status = $conn->query($query);
+	$conn->query($query);
+	$status = $conn->query($query_);
 	header('Location: uses.php');
 	setcookie('msg2','Xóa user thành công',time()+2);
  ?>
